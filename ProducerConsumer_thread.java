ed9893fee7301637a18cd56da1677bcab799e272
sisbhumi <87226560+sisbhumi@users.noreply.github.com>
2021-09-22 16:19:44 +0530

    Update ProducerConsumer_thread.java

--- ProducerConsumer_thread.java
+++ ProducerConsumer_thread.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ProducerConsumer_thread{
-
+//hello
     public static void main(String[] args) throws InterruptedException {
         Buffer buffer = new Buffer(2);
 
